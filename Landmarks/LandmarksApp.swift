5cf82d0f264543886d15ed48326d0cd31bfffb24
hankoon <galaxykhh@gmail.com>
2021-12-15 12:08:47 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김형한 on 2021/12/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
